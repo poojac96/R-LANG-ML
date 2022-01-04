8e0277f1f466f89e22398a4e1d701a69071aa5ee
poojac96 <87509471+poojac96@users.noreply.github.com>
2022-01-04 01:39:37 -0800

    INTR TO R PROGRAM (7)

---
+++ Introduction to R Programming.R
@@ -0,0 +1,11 @@
+#Introduction
+print('welcome to Analytics Class Rushi Sir')
+a<-print('welcome to Analytics Class Rushi Sir')
+# the <- sign can be used in place of = 
+a
+#Press Ctrl+Enter to run/execute the code
+
+#Example of plotting
+x=1:5
+y=6:10
+plot(x,y)
